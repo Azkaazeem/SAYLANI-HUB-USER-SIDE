725ce19f0ea9a3257edc8a4a080243006424b23e
Azka Azeem <azkaazeem804@gmail.com>
2026-02-22 15:38:53 +0500

    Setup complete

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import AuthHeader from "./Comp/AuthHeader";
+
+function App() {
+  return (
+    <>
+      <AuthHeader/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Comp/AuthHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthHeader = () => {
+  return (
+    <>
+        <h1>Sign Up</h1>
+        <div className='w-20 h-100% gradient-color'>fgfh</div>
+    </>
+  )
+}
+
+export default AuthHeader
